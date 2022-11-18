bcefa141ad345da818c7c3eab106d21b38873c1d
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-11-18 21:11:56 +0100

    Glow functionality

--- js/glow.js
+++ js/glow.js
@@ -3,8 +3,11 @@ import { main, cards } from './DOMelems.js';
 export default function glowOnHover(e) {
     for (const card of cards) {
         const rect = card.getBoundingClientRect();
-        // const x = e.clientX - rect.left
-        console.log(rect.left)
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty('--mouseX', `${x}px`);
+        card.style.setProperty('--mouseY', `${y}px`);
     }
 }
 
